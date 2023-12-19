de98ccbea08d10e25118c453dea14d40ad4331e3
Cameron McEvenue <mcevenc@gmail.com>
2023-12-19 18:15:09 -0500

    feat: added base eslint config

---
+++ packages/config-eslint/_base.js
@@ -0,0 +1,15 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	root: true,
+	extends: ['eslint:recommended'],
+	env: {
+		es2022: true
+	},
+	// Tell ESLint not to ignore dot-files, which are ignored by default.
+	ignorePatterns: ['!.*.js'],
+	// Global parser options.
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: 'module'
+	}
+};
